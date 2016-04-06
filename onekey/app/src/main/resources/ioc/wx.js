da0dee27647ee478a2c6a54ddf5b18937fd93d30
王贵源 <wkipy@hotmail.com>
2016-04-06 21:49:37 +0800

    111

--- onekey/app/src/main/resources/ioc/wx.js
+++ onekey/app/src/main/resources/ioc/wx.js
@@ -1,25 +1,25 @@
 var ioc = {
-		wxJsapiTicketStore : {
-			type : "org.nutz.weixin.at.impl.MemoryJsapiTicketStore"
-		},
-		wxApi : {
-			type : "org.nutz.weixin.impl.WxApi2Impl",
-			fields : {
-				token : {
-					java : "$config.get('token')"
-				},
-				appid : {
-					java : "$config.get('appid')"
-				},
-				appsecret : {
-					java : "$config.get('appsecret')"
-				},
-				encodingAesKey : {
-					java : "$config.get('encodingAesKey')"
-				},
-				jsapiTicketStore : {
-					refer : 'wxJsapiTicketStore'
-				}
+	wxJsapiTicketStore : {
+		type : "org.nutz.weixin.at.impl.MemoryJsapiTicketStore"
+	},
+	wxApi : {
+		type : "org.nutz.weixin.impl.WxApi2Impl",
+		fields : {
+			token : {
+				java : "$config.get('token')"
+			},
+			appid : {
+				java : "$config.get('appid')"
+			},
+			appsecret : {
+				java : "$config.get('appsecret')"
+			},
+			encodingAesKey : {
+				java : "$config.get('encodingAesKey')"
+			},
+			jsapiTicketStore : {
+				refer : 'wxJsapiTicketStore'
 			}
 		}
 	}
+}
