e3765f5968a38f92cebc2209f165c720ad050485
王贵源 <kerbores@gmail.com>
2016-06-25 10:32:20 +0800

    数据初始化逻辑

--- thunder/thunder-web/src/main/java/club/zhcs/thunder/ThunderSetup.java
+++ thunder/thunder-web/src/main/java/club/zhcs/thunder/ThunderSetup.java
@@ -15,38 +15,60 @@ import org.nutz.log.Logs;
 import org.nutz.mvc.NutConfig;
 import org.nutz.mvc.Setup;
 
+import club.zhcs.thunder.bean.acl.Role;
 import club.zhcs.thunder.bean.acl.User;
 import club.zhcs.thunder.bean.acl.User.Status;
+import club.zhcs.thunder.bean.acl.UserRole;
+import club.zhcs.thunder.biz.acl.RoleService;
+import club.zhcs.thunder.biz.acl.UserRoleService;
 import club.zhcs.thunder.biz.acl.UserService;
 
 /**
  * 
+ * 
+ * 
  * @author Kerbores(kerbores@gmail.com)
+ * 
  *
+ * 
  * @project thunder-web
+ * 
  *
+ * 
  * @file ThunderSetup.java
+ * 
  *
+ * 
  * @description 初始化
+ * 
  *
+ * 
  * @time 2016年3月8日 上午10:51:26
+ * 
  *
  */
 public class ThunderSetup implements Setup {
 	private static final Log log = Logs.get();
 
 	/*
+	 * 
 	 * (non-Javadoc)
+	 * 
+	 * 
 	 * 
 	 * @see org.nutz.mvc.Setup#destroy(org.nutz.mvc.NutConfig)
 	 */
 	@Override
 	public void destroy(NutConfig nc) {
 		// 初始化
+
 	}
 
 	/*
+	 * 
 	 * (non-Javadoc)
+	 * 
+	 * 
 	 * 
 	 * @see org.nutz.mvc.Setup#init(org.nutz.mvc.NutConfig)
 	 */
@@ -54,7 +76,7 @@ public class ThunderSetup implements Setup {
 	public void init(NutConfig nc) {
 
 		NutShiro.DefaultLoginURL = "/";
-		
+
 		if (!Charset.defaultCharset().name().equalsIgnoreCase(Encoding.UTF8)) {
 			log.warn("This project must run in UTF-8, pls add -Dfile.encoding=UTF-8 to JAVA_OPTS");
 		}
@@ -62,15 +84,22 @@ public class ThunderSetup implements Setup {
 		Ioc ioc = nc.getIoc();
 
 		Dao dao = ioc.get(Dao.class);
-		
+
 		ioc.get(NutQuartzCronJobFactory.class);// 触发任务
 
 		// 为全部标注了@Table的bean建表
+
 		Daos.createTablesInPackage(dao, getClass().getPackage().getName() + ".bean", false);
 		Daos.migration(dao, getClass().getPackage().getName() + ".bean", true, true);
 
 		// 超级管理员
+
 		UserService userService = ioc.get(UserService.class);
+
+		RoleService roleService = ioc.get(RoleService.class);
+
+		UserRoleService userRoleService = ioc.get(UserRoleService.class);
+
 		User surperMan = null;
 		if ((surperMan = userService.fetch(Cnd.where("name", "=", "admin"))) == null) {
 			surperMan = new User();
@@ -82,6 +111,23 @@ public class ThunderSetup implements Setup {
 			surperMan.setStatus(Status.ACTIVED);
 			surperMan = userService.save(surperMan);
 		}
+
+		Role admin = roleService.fetch(Cnd.where("name", "=", "admin"));
+		if (admin == null) {
+			admin = new Role();
+			admin.setInstalled(true);
+			admin.setName("admin");
+			admin.setDescription("超级管理员");
+			admin = roleService.save(admin);
+		}
+
+		if (userRoleService.fetch(Cnd.where("userId", "=", surperMan.getId()).and("roleId", "=", admin.getId())) == null) {
+			UserRole userRole = new UserRole();
+			userRole.setRoleId(admin.getId());
+			userRole.setUserId(surperMan.getId());
+			userRoleService.save(userRole);
+		}
+
 	}
 
 }
